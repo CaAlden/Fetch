192b477ed6283b09793c804d233218d4df080df6
Campbell Alden <alden.ca@husky.neu.edu>
2017-10-24 15:33:24 -0400

    fixup! Skeleton for vehicle controller abstraction

--- lib/navigation/vehicle_controller.py
+++ lib/navigation/vehicle_controller.py
@@ -4,28 +4,23 @@ This file contains the interface definition for a high level
 navigation controller.
 '''
 
-import abc
 import logging
 
 class VehicleController(object):
     ''' Navigation Controller interface base class.'''
-    __metaclass__ = abc.ABCMeta
 
     def __init__(self, vehicle_resource, logger=None):
         self._vehicle = vehicle_resource
         self._logger = logger
 
-    @abc.abstractmethod
     def navigate_to(self, gps_coord):
         ''' Navigate to the given GPS coordinate'''
         pass
 
-    @abc.abstractmethod
     def get_location(self):
         ''' Report the vehicles current location'''
         pass
 
-    @abc.abstractmethod
     def _handle_command(self, command):
         ''' Handle a basic command'''
         pass
