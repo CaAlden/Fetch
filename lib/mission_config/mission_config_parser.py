b147e233be1c117999a4dc65ccdc9aa07f3645ed
Campbell Alden <alden.ca@husky.neu.edu>
2017-11-01 17:24:17 -0400

    Mission_config: Basic configuration parser

---
+++ lib/mission_config/mission_config_parser.py
@@ -0,0 +1,71 @@
+import yaml
+
+def parseMissionConfig(missionPlanFilename):
+    ''' Parse a mission configuration from the mission plan file'''
+    with open(missionPlanFilename, 'r') as missionFile:
+        rawConfig = yaml.load(missionFile)
+
+    validateFormat(rawConfig)
+    return rawConfig
+
+
+def validateTitle(title):
+    if title is None:
+        return "\n Must specify a title"
+
+def validateType(typeValue):
+    typeValue = typeValue
+    expectedTypes = ["deploy", "retrieve", "test"]
+    if typeValue not in expectedTypes:
+        return "\n- Type must be one of " + str(expectedTypes) + " got " + typeValue
+
+def validateDescription(description):
+    pass
+
+def validateWaypoints(waypoints):
+
+    if waypoints is None or len(waypoints) < 1:
+        return "\n- Must specify at least 1 waypoint"
+    else:
+        completeReport = ''
+        for latLon in waypoints:
+            output = validateGPSCoord(latLon)
+            if output is not None:
+                completeReport += "\n" + output
+        if len(completeReport) > 0:
+            return completeReport
+
+def validateGPSCoord(latLon):
+    try:
+        lat, lon = tuple(map(lambda x: float(x.strip()), latLon.split(',')))
+    except ValueError:
+        return "- Malformed gps coordinate: " + latLon
+    errStr = None
+    if abs(lat) > 90:
+        errStr = "- Latitude out of bounds: {}".format(lat)
+
+    if abs(lon) > 180:
+        if errStr is None:
+            errStr = "- Longitude out of bounds: {}".format(lon)
+        else:
+            errStr += "\n- Longitude out of bounds: {}".format(lon)
+
+    return errStr
+
+VALIDATION_MAP  = {
+    'title': validateTitle,
+    'description': validateDescription,
+    'waypoints': validateWaypoints,
+    'type': validateType
+}
+
+def validateFormat(config, warnings=False):
+    warnings = ''
+    for key, value in config.items():
+        if key.lower() in VALIDATION_MAP:
+            out = VALIDATION_MAP[key.lower()](value)
+            if out is not None:
+                warnings += out
+
+    if len(warnings) > 0:
+        raise ValueError("Invalid Mission Config: {}".format(warnings))
