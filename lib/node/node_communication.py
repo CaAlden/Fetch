e174b161c7acc5d34a848a0e9f552e2370d3bdcc
ollie fishstein <ofishstein@gmail.com>
2017-11-27 22:24:13 -0500

    updated node code

--- lib/node/node_communication.py
+++ lib/node/node_communication.py
@@ -1,41 +1,52 @@
 import json
 import serial
-import sys, time, datetime 		
+import sys, time, datetime 	
+import gps_util	
 
 
 SERIAL_PORT = “/dev/ttyAMA0”
 BAUDRATE = 9600
 
-# decodes data from xbee from json
-def parse_recv_data(data):
-	recv_data = json.loads(data)
+class NodeHub():
+	def __init__(self):
+		self.serial = None
 
-# encodes data for xbee to json
-def encode_send_data(data):
-	return json.dumps(data)
+	# decodes data from xbee from json
+	def parse_recv_data(self, data):
+		return json.loads(data)
 
-# TODO figure out what to send if we want to send anything
-def build_send_data():
-	pass
+	# encodes data for xbee to json
+	def encode_send_data(self, data):
+		return json.dumps(data)
 
-# TODO define what this should do 
-def handle_recv_data(data):
-	pass
+	# TODO figure out what to send if we want to send anything
+	def build_send_data(self):
+		pass
 
-def connect_xbee():
-	# connect to xbee over serial 
-	# TODO define timeout better to match node xbees
-	serial_connection = serial.Serial(SERIAL_PORT, BAUDRATE, timeout = 1)
+	def handle_recv_data(self, data):
+		print("node: " + data["node"])
+		print("gps: " + data["gps"])
+		coord = gps_util.GPSCoord.from_GPGLL_string(data["gps"])
+		print("lat: " + str(coord.latitutde))
+		print("lon: " + str(coord.longitude))
 
-	while True:
-		recv_data = serial_connection.readline()
+	def connect_xbee(self):
+		# connect to xbee over serial 
+		# TODO define timeout better to match node xbees
+		self.serial = serial.Serial(SERIAL_PORT, BAUDRATE, timeout = 10)
 
-		if recv_data is not None:
-			recv_data = recv_data.strip('\n')
-			processed_data = parse_recv_data(recv_data)
-			handle_recv_data(processed_data)
+	def get_data(self):
+		while True:
+			recv_data = serial_connection.readline()
 
-	serial_connection.close()
+			if recv_data is not None:
+				recv_data = recv_data.strip('\n')
+				processed_data = self.parse_recv_data(recv_data)
+				self.handle_recv_data(processed_data)
+
+	def close(self):
+		self.serial.close()
+
 
 	
 
--- node_code/node.ino
+++ node_code/node.ino
@@ -1,12 +1,73 @@
+#include <SoftwareSerial.h>
+
 #define BAUDRATE 9600
 #define DELAY 1000
+#define LED_PIN 10
+#define STATUS_LED 12
+
+SoftwareSerial gpsSerial(5, 6); // RX, TX
+char ledState;
+String gpsString;
+String xbeeString;
+
+#define NODE_NUMBER 1 //change this manually for each node :(
 
 void setup() {
+	// open up serial communication
 	Serial.begin(BAUDRATE);
+
+	while (!Serial) {
+		; // wait for serial port to connect. Needed for native USB port only
+	}
+
+	gpsSerial.begin(BAUDRATE);
+	gpsSerial.write(0xFF);
+
+	pinMode(LED_PIN, OUTPUT); // red LED
+	pinMode(STATUS_LED, OUTPUT);
 }
 
 void loop() {
-	// TODO read some data and build json to send
-	Serial.println("{"test": "Hello World"}");
+
+	triggerLed();
+	readXbee();	
+	triggerLed();
+	readGPS();
+	triggerLed();
+	sendXbeeMessage();
+	triggerLed();
 	delay(DELAY);
+}
+
+void sendXbeeMessage() {
+	# send json with node number and gpscoordinates
+	Serial.println("{\"node\":\"" + NODE_NUMBER + "\", \"gps\":\"" + gpsString + "\"}");
+}
+
+void readXbee() {
+	xbeeString = "";
+	Serial.listen()
+
+	if (Serial.available() > 0) {
+		char nextChar = Serial.read();
+		xbeeString += nextChar;
+	}
+}
+
+void readGPS() {
+	gpsString = "";
+	gpsSerial.listen()
+
+	if (gpsSerial.available() > 0) {
+		char nextChar = gpsSerial.read();
+		if (char != '\n') {
+			gpsString += nextChar;
+		}
+	}
+	
+}
+
+void triggerLed() {
+	digitalWrite(STATUS_LED,(ledState) ? HIGH : LOW);
+	ledState = !ledState;
 }
